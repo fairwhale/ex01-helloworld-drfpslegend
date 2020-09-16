9e31b8a3601ea1669fa7e8350de0144b56ba4a12
fairwhale <108230695+fairwhale@users.noreply.github.com>
2020-09-16 17:57:26 +0000

    Fix formatting error

--- mod-5-algo-trading-backend/app/controllers/api/v1/positions_controller.rb
+++ mod-5-algo-trading-backend/app/controllers/api/v1/positions_controller.rb
@@ -1,2 +1,5 @@
 class Api::V1::PositionsController < ApplicationController
-  before_action :find_position, only: [:up
+  before_action :find_position, only: [:update]
+
+  def index
+    @positions =
