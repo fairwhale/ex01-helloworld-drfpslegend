f08750548dd496c88dce5739496aaf3fd88c2122
fairwhale <108230695+fairwhale@users.noreply.github.com>
2020-11-11 12:19:01 +0000

    Work on import module

--- mod-5-algo-trading-backend/app/serializers/account_serializer.rb
+++ mod-5-algo-trading-backend/app/serializers/account_serializer.rb
@@ -2,4 +2,6 @@ class AccountSerializer < ActiveModel::Serializer
   attributes :id, :username, :password, :email, :cash_deposited
   has_many :positions
   has_many :orders
-  has_many :algori
+  has_many :algorithms
+
+end
