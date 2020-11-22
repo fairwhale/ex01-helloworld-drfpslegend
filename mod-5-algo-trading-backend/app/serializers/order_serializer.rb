90bb34b69e39af0f77c90acc0323d90da7235759
fairwhale <108230695+fairwhale@users.noreply.github.com>
2020-11-22 07:51:19 +0000

    Update shippable.triggers.yml

--- mod-5-algo-trading-backend/app/serializers/order_serializer.rb
+++ mod-5-algo-trading-backend/app/serializers/order_serializer.rb
@@ -1,2 +1,2 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :account_id, :quantity, :price, :limit, :t
+  attributes :id, :account_id, :quantity, :price, :limit, :trading_pair, :usd_id, :pos
