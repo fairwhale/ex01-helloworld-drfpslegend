2512f8d8ddbfac0525302ba1fd205f7e696132ca
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-05-29 20:26:22 +0000

    Update location.md

--- mod-5-algo-trading-backend/config/environments/development.rb
+++ mod-5-algo-trading-backend/config/environments/development.rb
@@ -2,4 +2,5 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for de
+  # every request. This slows down response time but is perfect for development
+  # since you don't h
