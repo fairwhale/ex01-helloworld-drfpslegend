5263e8e388efd5cb70ce7cf5cafdc2602d123cae
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-06-12 19:41:06 +0000

    Bump 0.1.0

--- mod-5-algo-trading-backend/config/environments/development.rb
+++ mod-5-algo-trading-backend/config/environments/development.rb
@@ -16,4 +16,5 @@ Rails.application.configure do
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
-    config.cache_store = :memory_
+    config.cache_store = :memory_store
+    config.public_fil
