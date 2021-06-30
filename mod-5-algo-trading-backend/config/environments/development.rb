6f62695280be8eb8a5c6c597bd9fe04c9881af10
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-06-30 20:09:21 +0000

    Google export

--- mod-5-algo-trading-backend/config/environments/development.rb
+++ mod-5-algo-trading-backend/config/environments/development.rb
@@ -32,4 +32,6 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
-  config.active_support
+  config.active_support.deprecation = :log
+
+  # Raise an error o
