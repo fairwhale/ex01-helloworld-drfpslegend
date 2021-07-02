2280ec94a572d1d0a37f90806b52903a53c8abf6
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-07-02 07:05:15 +0000

    Version 0

--- mod-5-algo-trading-backend/config/environments/development.rb
+++ mod-5-algo-trading-backend/config/environments/development.rb
@@ -34,4 +34,5 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pendi
+  # Raise an error on page load if there are pending migrations.
+  config.act
