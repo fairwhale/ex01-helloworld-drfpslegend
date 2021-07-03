c05ef0ae34d7e6994f270db4faa9576c83b27994
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-07-03 06:45:41 +0000

    Gerana, nomdefespgrulab

--- mod-5-algo-trading-backend/config/environments/development.rb
+++ mod-5-algo-trading-backend/config/environments/development.rb
@@ -35,4 +35,7 @@ Rails.application.configure do
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :pa
+  config.active_record.migration_error = :page_load
+
+
+  # Raises error
