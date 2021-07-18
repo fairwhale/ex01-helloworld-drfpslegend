d536abc1345430884f34a568730384c60a7d90da
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-07-18 14:05:37 +0000

    Indent code

--- mod-5-algo-trading-backend/config/environments/development.rb
+++ mod-5-algo-trading-backend/config/environments/development.rb
@@ -43,4 +43,4 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  co
+  config.file_watcher = ActiveSupport
