4d44ba7a1605b98f0405f1021a59bad759727594
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-07-18 11:00:54 +0000

    Updated todos.

--- mod-5-algo-trading-backend/config/environments/development.rb
+++ mod-5-algo-trading-backend/config/environments/development.rb
@@ -42,4 +42,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This fea
+  # routes, locales, etc. This feature depends on the listen gem.
+  co
