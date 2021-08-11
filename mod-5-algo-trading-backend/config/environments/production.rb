7ef49cf2a76ec347cd7bb9f2dc3645f9ce76c832
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-08-11 07:31:10 +0000

    Reorganize directory structure

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -7,4 +7,4 @@ Rails.application.configure do
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
-  # Rake tasks 
+  # Rake tasks automatically ignore this optio
