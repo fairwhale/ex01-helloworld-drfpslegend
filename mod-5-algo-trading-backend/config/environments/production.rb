6b65cd55af2412c60f4369f4867ed1873e1f2663
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-08-11 11:38:16 +0000

    Fix install instructions

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -7,4 +7,5 @@ Rails.application.configure do
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this optio
+  # Rake tasks automatically ignore this option for performance.
+  config.ea
