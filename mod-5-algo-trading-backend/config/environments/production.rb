c1351cc9ca6d77031cb3533aa1e64dfbf623598a
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-08-14 19:13:12 +0000

    Added travis build status to README

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -12,4 +12,4 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_c
+  config.action_controller.perform_caching =
