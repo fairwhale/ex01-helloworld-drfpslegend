fed574206edbcf371b14687d820791ae383c1206
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-09-02 13:20:02 +0000

    next version

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -15,4 +15,6 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
-  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]`
+  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
+  # `config/secrets.yml.key`.
+  config.re
