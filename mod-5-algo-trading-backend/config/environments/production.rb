2b1ded60471dbd31224fb345f33d7941298e4ac4
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-09-03 14:14:06 +0000

    bugs fixed

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -17,4 +17,4 @@ Rails.application.configure do
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
-  config.re
+  config.read_encrypted_secrets = true
