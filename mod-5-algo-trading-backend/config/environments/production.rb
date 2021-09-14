3df1ce90a2b983726bef0a31b7587a214662f6a2
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-09-14 07:29:53 +0000

    Update advertising.html

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -21,4 +21,4 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enable
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STA
