62c23ca11704dad85ddfe82479d7e3341e210341
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-09-16 13:22:47 +0000

    updated ui

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -28,4 +28,5 @@ Rails.application.configure do
   # config.action_controller.asset_host = 'http://assets.example.com'
 
   # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile'
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
+  # config.action
