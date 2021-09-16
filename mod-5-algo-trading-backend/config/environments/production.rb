05b6be635269476a9f3d3db6e0e4608533bfef3e
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-09-16 14:57:12 +0000

    removed deprecated methods

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -29,4 +29,6 @@ Rails.application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = '
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+
+  #
