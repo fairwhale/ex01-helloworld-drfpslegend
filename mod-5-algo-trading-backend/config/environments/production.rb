c4c7531d0d6562615f0de1db1e06c6eed35d24b8
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-11-04 22:02:17 +0000

    Formatting cleanup

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -58,4 +58,4 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  # Enable
+  # Enable locale fallbacks for I18n (ma
