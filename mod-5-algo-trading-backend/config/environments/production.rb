f9f96f94d7cbc22a97656fd1605fafe2ffacc2f6
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-11-08 05:29:48 +0000

    ./scripts/bootstrap-roles.sh && ./scripts/update-tests.sh

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -60,4 +60,6 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = tru
+  config.i18n.fallbacks = true
+
+  # Send deprecation notices t
