234dedca8df636a1d0b4a9d026460d4bf13f0522
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-11-08 09:24:26 +0000

    Update tmux

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -62,4 +62,5 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
-  # Send deprecation notices t
+  # Send deprecation notices to registered listeners.
+  config.acti
