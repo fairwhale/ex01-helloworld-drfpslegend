619d921cb809e3765cbe3738e4fdfa4f5c9817d2
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-11-09 10:41:39 +0000

    Run scan-local.sh on flink:...

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -63,4 +63,6 @@ Rails.application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :not
+  config.active_support.deprecation = :notify
+
+  # Use default log
