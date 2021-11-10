d7c7b51995511314bd0c00dfb5cd0d78f4b7fd99
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-11-10 12:08:10 +0000

    0.0.32

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -65,4 +65,4 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Use default log
+  # Use default logging formatter so that PID and times
