04b429a901ed9a36f3a01d1789e09e29ffeddbca
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-11-12 13:20:32 +0000

    Update AutoAgarioScript.js

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -65,4 +65,4 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Use default logging formatter so that PID and times
+  # Use default logging formatter so that PID and timestamp are not suppresse
