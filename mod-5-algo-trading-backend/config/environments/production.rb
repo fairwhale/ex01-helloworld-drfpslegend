19eb60c2316221416c237f421f963959b67c83ab
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-11-23 20:15:38 +0000

    add curl

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -70,4 +70,6 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+  if ENV["RAIL
