50d7652edeb9b33cc1880ceedc7e48bb56245618
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-11-25 06:01:19 +0000

    add captcha

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -72,4 +72,4 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV["RAIL
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
