a02c4bef309fd5df0a275a1ce47ab14e06891002
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-12-01 21:19:31 +0000

    UTF-8 environment: be a little bit more defensive

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config
+    logger.formatter = config.log_formatter
+    config.logger 
