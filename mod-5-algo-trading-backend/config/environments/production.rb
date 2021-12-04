e513d2a112faff9d06a965b26db7cfa86e3cb7dc
fairwhale <108230695+fairwhale@users.noreply.github.com>
2021-12-04 07:33:12 +0000

    / ‘tidal/spicule/#livestems.tidal#’

--- mod-5-algo-trading-backend/config/environments/production.rb
+++ mod-5-algo-trading-backend/config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogg
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+
+  # 
