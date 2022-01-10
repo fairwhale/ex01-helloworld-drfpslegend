1a62267d323c6e5800b6128ece114b90fd055373
fairwhale <108230695+fairwhale@users.noreply.github.com>
2022-01-10 21:05:30 +0000

    Add license scan report and status

--- mod-5-algo-trading-backend/config/environments/test.rb
+++ mod-5-algo-trading-backend/config/environments/test.rb
@@ -19,4 +19,5 @@ Rails.application.configure do
   }
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local 
+  config.consider_all_requests_local       = true
+  config.action_contro
