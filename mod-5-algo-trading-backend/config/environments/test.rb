9943209a1dacd0bc51fc37812470534452ac6543
fairwhale <108230695+fairwhale@users.noreply.github.com>
2022-01-13 11:03:10 +0000

    Fixed dockerfile

--- mod-5-algo-trading-backend/config/environments/test.rb
+++ mod-5-algo-trading-backend/config/environments/test.rb
@@ -22,4 +22,4 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Raise exceptions ins
+  # Raise exceptions instead of rendering exception template
