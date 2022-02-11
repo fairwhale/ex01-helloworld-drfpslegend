acc4068894fc5075ae6ad4c5f15786a87f0b39c1
fairwhale <108230695+fairwhale@users.noreply.github.com>
2022-02-11 10:31:33 +0000

    update ignore patterns

--- mod-5-algo-trading-backend/config/environments/test.rb
+++ mod-5-algo-trading-backend/config/environments/test.rb
@@ -31,4 +31,5 @@ Rails.application.configure do
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries a
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_m
