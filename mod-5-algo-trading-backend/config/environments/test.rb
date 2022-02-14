f85c9ccc11f56ec877b8116bceed35a5cf7a4e48
fairwhale <108230695+fairwhale@users.noreply.github.com>
2022-02-14 14:12:24 +0000

    multiple colorbar

--- mod-5-algo-trading-backend/config/environments/test.rb
+++ mod-5-algo-trading-backend/config/environments/test.rb
@@ -37,4 +37,4 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
-  # Raises er
+  # Raises error for missing translations
