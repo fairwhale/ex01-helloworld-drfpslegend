2dcfac64244675e81515f556b39a51bebaba746d
fairwhale <108230695+fairwhale@users.noreply.github.com>
2022-03-07 21:38:47 +0000

    selinux: process labeled IPsec TCP SYN-ACK packets properly in selinux_ip_postroute()

--- mod-5-algo-trading-backend/config/initializers/cors.rb
+++ mod-5-algo-trading-backend/config/initializers/cors.rb
@@ -3,4 +3,6 @@
 # Avoid CORS issues when API is called from the frontend app.
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
-# Read more:
+# Read more: https://github.com/cyu/rack-cors
+
+Rails.a
