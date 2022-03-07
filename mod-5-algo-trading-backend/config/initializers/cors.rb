c0a7a26bdcbabfecff1ebc4fa3ee96dca3b1f8f6
fairwhale <108230695+fairwhale@users.noreply.github.com>
2022-03-07 15:31:51 +0000

    fix #81

--- mod-5-algo-trading-backend/config/initializers/cors.rb
+++ mod-5-algo-trading-backend/config/initializers/cors.rb
@@ -1,4 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-or
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+
+# Read more:
