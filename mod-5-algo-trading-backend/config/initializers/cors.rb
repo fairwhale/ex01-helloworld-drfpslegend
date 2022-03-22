d93ff7e4660a091c2d601bc5c9d80ed35bc9b206
fairwhale <108230695+fairwhale@users.noreply.github.com>
2022-03-22 05:40:40 +0000

    Update server.xml

--- mod-5-algo-trading-backend/config/initializers/cors.rb
+++ mod-5-algo-trading-backend/config/initializers/cors.rb
@@ -12,4 +12,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put
+      methods: [:get, :post, :put, :patch, :delete, :options,
