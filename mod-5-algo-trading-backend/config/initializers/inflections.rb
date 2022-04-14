2769f61d17da728b960e75931fd1693d7a4df592
fairwhale <108230695+fairwhale@users.noreply.github.com>
2022-04-14 21:20:03 +0000

    Auto-commit: Guillego's Server

--- mod-5-algo-trading-backend/config/initializers/inflections.rb
+++ mod-5-algo-trading-backend/config/initializers/inflections.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-# Add new inflection rules using the following forma
+# Add new inflection rules using the following format. Inflections
+# are locale speci
