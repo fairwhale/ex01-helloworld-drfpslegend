006e99f6114a6fb75fa133a9ea74cad8059a8b32
fairwhale <108230695+fairwhale@users.noreply.github.com>
2022-04-14 22:00:44 +0000

    added post

--- mod-5-algo-trading-backend/config/initializers/inflections.rb
+++ mod-5-algo-trading-backend/config/initializers/inflections.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Add new inflection rules using the following format. Inflections
-# are locale speci
+# are locale specific, and you may define rules for 
