5e426a76afd705fe700653c69ada0921fcf9d035
fairwhale <108230695+fairwhale@users.noreply.github.com>
2022-05-03 19:17:21 +0000

    Update some deps

--- mod-5-algo-trading-backend/config/initializers/inflections.rb
+++ mod-5-algo-trading-backend/config/initializers/inflections.rb
@@ -2,4 +2,4 @@
 
 # Add new inflection rules using the following format. Inflections
 # are locale specific, and you may define rules for as many different
-# locales as you w
+# locales as you wish. All of these example
