7238fce2a4e1e8b6e82f0bf7b5dd4f61c42afeca
fairwhale <108230695+fairwhale@users.noreply.github.com>
2022-05-28 06:36:14 +0000

    modify test case

--- mod-5-algo-trading-backend/config/initializers/inflections.rb
+++ mod-5-algo-trading-backend/config/initializers/inflections.rb
@@ -6,4 +6,5 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
-#   inflect.irregular 'person', 'peo
+#   inflect.irregular 'person', 'people'
+#   inflect.uncountab
