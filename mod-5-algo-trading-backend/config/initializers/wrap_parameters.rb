8f85e91a12b879fc7b71bafb5e0eb2ac9134c060
fairwhale <108230695+fairwhale@users.noreply.github.com>
2022-07-28 19:55:58 +0000

    nfs4.1: properly handle ENOTSUP in SECINFO_NO_NAME

--- mod-5-algo-trading-backend/config/initializers/wrap_parameters.rb
+++ mod-5-algo-trading-backend/config/initializers/wrap_parameters.rb
@@ -9,4 +9,5 @@ ActiveSupport.on_load(:action_controller) do
 end
 
 # To enable root element in JSON for ActiveRecord objects.
-# ActiveSupport.on_load(:active_record) d
+# ActiveSupport.on_load(:active_record) do
+#   self.include_root_in_j
