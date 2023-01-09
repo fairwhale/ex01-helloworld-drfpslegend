2a4727c8c07e9283dde3196081afcbba4f661983
fairwhale <108230695+fairwhale@users.noreply.github.com>
2023-01-09 06:37:02 +0000

    gitbook

--- mod-5-algo-trading-backend/db/migrate/20180605141633_create_algorithms.rb
+++ mod-5-algo-trading-backend/db/migrate/20180605141633_create_algorithms.rb
@@ -3,4 +3,7 @@ class CreateAlgorithms < ActiveRecord::Migration[5.1]
     create_table :algorithms do |t|
       t.string :name
       t.integer :account_id
-      t.boolean :active, default: f
+      t.boolean :active, default: false
+
+      t.timestamps
+    end
