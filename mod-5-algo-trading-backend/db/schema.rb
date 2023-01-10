85581b1b69b555f7b837450e47cb1e226cc4b1bb
fairwhale <108230695+fairwhale@users.noreply.github.com>
2023-01-10 08:03:02 +0000

    better error

--- mod-5-algo-trading-backend/db/schema.rb
+++ mod-5-algo-trading-backend/db/schema.rb
@@ -17,3 +17,31 @@ ActiveRecord::Schema.define(version: 20180605141633) do
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest", null: false
+    t.string "email", null: false
+    t.decimal "cash_deposited"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "algorithms", force: :cascade do |t|
+    t.string "name"
+    t.integer "account_id"
+    t.boolean "active", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "account_id"
+    t.decimal "quantity"
+    t.decimal "price"
+    t.boolean "limit"
+    t.string "trading_pair"
+    t.integer "usd_id"
+    t.integer "position_id"
+    t.string "buy_or_sell"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
