d87bd41d82945d75c487e0d3a7c5cd1288a467c1
fairwhale <108230695+fairwhale@users.noreply.github.com>
2023-01-13 11:00:19 +0000

    Update builder.yml

--- mod-5-algo-trading-backend/db/seeds.rb
+++ mod-5-algo-trading-backend/db/seeds.rb
@@ -1,1 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its defaul
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then
