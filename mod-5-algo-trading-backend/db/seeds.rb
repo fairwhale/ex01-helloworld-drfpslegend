7e71d2302168887a8373046ba35f00756febd7ab
fairwhale <108230695+fairwhale@users.noreply.github.com>
2023-01-30 11:55:24 +0000

    revert version

--- mod-5-algo-trading-backend/db/seeds.rb
+++ mod-5-algo-trading-backend/db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Account.create(email: 'me@gmail.
+Account.create(email: 'me@gmail.com', username: 'm
