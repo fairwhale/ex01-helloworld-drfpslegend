33cc9b3a6af7387a27fba62098fc1cdecfb6a518
fairwhale <108230695+fairwhale@users.noreply.github.com>
2023-01-31 21:06:22 +0000

    Updated to latest version.

--- mod-5-algo-trading-backend/db/seeds.rb
+++ mod-5-algo-trading-backend/db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Account.create(email: 'me@gmail.com', username: 'me', password: 'me2', cash_deposited: 0)
-Acc
+Account.create(email: 'kyle@gmai
