4d860135e31f54729246626dde9d3358472f81c2
fairwhale <108230695+fairwhale@users.noreply.github.com>
2023-02-16 05:36:28 +0000

    org-supplements.el

--- mod-5-algo-trading-backend/db/seeds.rb
+++ mod-5-algo-trading-backend/db/seeds.rb
@@ -13,4 +13,5 @@ Position.create(account_id: 1, quantity: 0, trading_pair: 'LTC-USD')
 Position.create(account_id: 1, quantity: 0, trading_pair: 'BCH-USD')
 Position.create(account_id: 1, quantity: 1000, trading_pair: 'USD')
 Position.create(account_id: 2, quantity: 1.0034, trading_pair: 'BTC-USD')
-Position.create(account_id: 2, quantity: 2.34, trading
+Position.create(account_id: 2, quantity: 2.34, trading_pair: 'ETH-USD')
+Position
