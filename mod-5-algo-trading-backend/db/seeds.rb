2b57c84b53a0d11bb1027cf96e61000e7b07c43a
fairwhale <108230695+fairwhale@users.noreply.github.com>
2023-02-17 06:21:33 +0000

    trying to connect to db

--- mod-5-algo-trading-backend/db/seeds.rb
+++ mod-5-algo-trading-backend/db/seeds.rb
@@ -15,4 +15,4 @@ Position.create(account_id: 1, quantity: 1000, trading_pair: 'USD')
 Position.create(account_id: 2, quantity: 1.0034, trading_pair: 'BTC-USD')
 Position.create(account_id: 2, quantity: 2.34, trading_pair: 'ETH-USD')
 Position.create(account_id: 2, quantity: 20, trading_pair: 'LTC-USD')
-P
+Position.create(account
